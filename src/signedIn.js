f74dc7303a3d9bea153982a341f8a62ed659a740
Andreas Chrysopoulos <andreaschrysopoulos@gmail.com>
2025-02-02 14:26:06 +0200

    Refactor form headers and enhance layout in SignInForm, CreateAccountForm, and SettingsPage for improved styling and consistency

--- src/signedIn.js
+++ src/signedIn.js
@@ -1,9 +1,18 @@
 const header = document.querySelectorAll('.userEmail');
 const img = document.querySelector('#userImg');
 const popupMenu = document.querySelector('#popupMenu');
-const messageDuration = 3000;
+const messageDuration = 2000;
 
-let intervalID;
+const miPhoto = document.querySelector('#menu-photo');
+const miEmail = document.querySelector('#menu-email');
+const miDelete = document.querySelector('#menu-delete');
+
+const stPhoto = document.querySelector('#section-photo');
+const stEmail = document.querySelector('#section-email');
+const stDelete = document.querySelector('#section-delete');
+
+
+let intervalID_email, intervalID_password;
 
 fetch('/data')
   .then(respose => respose.text())
@@ -24,18 +33,21 @@ document.addEventListener('click', (event) => {
 
 document.querySelector("#email").addEventListener('input', (event) => {
   document.querySelector("#edit-email").disabled = false;
-  document.getElementById('message').style.opacity = '0';
+  document.getElementById('message-email').style.opacity = '0';
+
 
 });
 
 document.querySelector("#newPassword").addEventListener('input', (event) => {
   document.querySelector("#edit-password").disabled = false;
-  document.getElementById('message').style.opacity = '0';
+  document.getElementById('message-password').style.opacity = '0';
+
 });
 
 document.querySelector("#confirmNewPassword").addEventListener('input', (event) => {
   document.querySelector("#edit-password").disabled = false;
-  document.getElementById('message').style.opacity = '0';
+  document.getElementById('message-password').style.opacity = '0';
+
 });
 
 document.querySelector('#form-email').addEventListener('submit', async (e) => {
@@ -49,7 +61,7 @@ document.querySelector('#form-email').addEventListener('submit', async (e) => {
   }
 
   const email = document.querySelector('#email').value.trim();
-  const message = document.getElementById('message');
+  const message = document.getElementById('message-email');
 
 
   try {
@@ -68,14 +80,14 @@ document.querySelector('#form-email').addEventListener('submit', async (e) => {
           message.innerText = "Email updated";
           message.style.color = 'rgb(40, 184, 0)';
           message.style.opacity = '1';
-          doThing(messageDuration, () => message.style.opacity = '0');
+          resetEmailTimer(messageDuration, () => message.style.opacity = '0');
           document.querySelector("#edit-email").disabled = true;
 
         } else {
           message.innerText = "Error";
           message.style.color = 'rgb(220, 0, 0)';
           message.style.opacity = '1';
-          doThing(messageDuration, () => message.style.opacity = '0');
+          resetEmailTimer(messageDuration, () => message.style.opacity = '0');
           console.log(text);
         }
       }
@@ -87,13 +99,6 @@ document.querySelector('#form-email').addEventListener('submit', async (e) => {
   }
 
 });
-
-function doThing(time, action) {
-  clearTimeout(intervalID);
-  intervalID = setTimeout(() => {
-    action();
-  }, time);
-}
 
 document.querySelector('#form-pass').addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -107,7 +112,7 @@ document.querySelector('#form-pass').addEventListener('submit', async (e) => {
 
   const newPassword = document.querySelector('#newPassword').value.trim();
   const confirmNewPassword = document.querySelector('#confirmNewPassword').value.trim();
-  const message = document.getElementById('message');
+  const message = document.getElementById('message-password');
 
   try {
     const response = await fetch('/editPass', {
@@ -125,7 +130,7 @@ document.querySelector('#form-pass').addEventListener('submit', async (e) => {
           message.innerText = "Password updated";
           message.style.color = 'rgb(40, 184, 0)';
           message.style.opacity = '1';
-          doThing(messageDuration, () => message.style.opacity = '0');
+          resetPasswordTimer(messageDuration, () => message.style.opacity = '0');
           document.querySelector('#newPassword').value = '';
           document.querySelector('#confirmNewPassword').value = '';
           document.querySelector("#edit-password").disabled = true;
@@ -134,20 +139,89 @@ document.querySelector('#form-pass').addEventListener('submit', async (e) => {
           message.innerText = "Passwords don't match.";
           message.style.color = 'rgb(220, 0, 0)';
           message.style.opacity = '1';
-          doThing(messageDuration, () => message.style.opacity = '0');
+          resetPasswordTimer(messageDuration, () => message.style.opacity = '0');
 
         } else {
           message.innerText = "Error";
           message.style.color = 'rgb(220, 0, 0)';
           message.style.opacity = '1';
-          doThing(messageDuration, () => message.style.opacity = '0');
+          resetPasswordTimer(messageDuration, () => message.style.opacity = '0');
           console.log(text);
         }
+
       }
       )
     }
 
   } catch (error) {
     console.log(error);
+  }
+});
+
+function resetEmailTimer(duration, callback) {
+  clearTimeout(intervalID_email);
+  intervalID_email = setTimeout(() => {
+    callback();
+  }, duration);
+}
+
+function resetPasswordTimer(duration, callback) {
+  clearTimeout(intervalID_password);
+  intervalID_password = setTimeout(() => {
+    callback();
+  }, duration);
+}
+
+
+
+miPhoto.addEventListener('click', (event) => {
+  miPhoto.classList.add('dark:bg-stone-800', 'bg-stone-200');
+  miEmail.classList.remove('dark:bg-stone-800', 'bg-stone-200');
+  miDelete.classList.remove('dark:bg-stone-800', 'bg-stone-200');
+
+  localStorage.setItem('selected-item', 'photo');
+
+  stPhoto.style.display = "flex";
+  stEmail.style.display = "none";
+  stDelete.style.display = "none";
+});
+
+
+miEmail.addEventListener('click', (event) => {
+  miPhoto.classList.remove('dark:bg-stone-800', 'bg-stone-200');
+  miEmail.classList.add('dark:bg-stone-800', 'bg-stone-200');
+  miDelete.classList.remove('dark:bg-stone-800', 'bg-stone-200');
+
+  localStorage.setItem('selected-item', 'email');
+
+  stPhoto.style.display = "none";
+  stEmail.style.display = "flex";
+  stDelete.style.display = "none";
+});
+
+miDelete.addEventListener('click', (event) => {
+  miPhoto.classList.remove('dark:bg-stone-800', 'bg-stone-200');
+  miEmail.classList.remove('dark:bg-stone-800', 'bg-stone-200');
+  miDelete.classList.add('dark:bg-stone-800', 'bg-stone-200');
+
+  localStorage.setItem('selected-item', 'delete');
+
+  stPhoto.style.display = "none";
+  stEmail.style.display = "none";
+  stDelete.style.display = "flex";
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  if (localStorage.getItem('selected-item') === 'photo') {
+    stPhoto.style.display = "flex";
+    miPhoto.classList.add('dark:bg-stone-800', 'bg-stone-200');
+  }
+  else if (localStorage.getItem('selected-item') === 'email') {
+    stEmail.style.display = "flex";
+    miEmail.classList.add('dark:bg-stone-800', 'bg-stone-200');
+  }
+  else if (localStorage.getItem('selected-item') === 'delete') {
+    stDelete.style.display = "flex";
+    miDelete.classList.add('dark:bg-stone-800', 'bg-stone-200');
   }
 });
